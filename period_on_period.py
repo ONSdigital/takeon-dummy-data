8ec49312f450e56c0ecb7bb2031306900b48fa79
RyanKilbane <RyanKilbane92@hotmail.com>
2019-08-08 11:47:09 +0100

    Added POP class and binary search method to class

--- period_on_period.py
+++ period_on_period.py
@@ -1,6 +1,14 @@
 from . import response_factory
 
-class PeriodOnPeriod(response_factory.ContributorResponse):
+class PeriodOnPeriod(response_facory.ContributorResponse):
 
-    def make_popm_pass(self):
-        pass
+    def b_search(self, match: int, search: list):
+             mid_point = math.floor(len(search)/2)
+             if search[mid_point]["reference"] == match:
+                 return search[mid_point]
+             elif search[mid_point]["reference"] > match:
+                 return b_search(search[:mid_point], match)
+             else:
+                 return b_search(search[mid_point:], match)
+
+
