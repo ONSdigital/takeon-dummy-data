3f4bb729f346c901a73ffc73581081ef1099f200
RyanKilbane <RyanKilbane92@hotmail.com>
2019-08-10 21:44:46 +0100

    Started working on recursively searching a dict to format the contributor data

--- insert_into_db/format_data.py
+++ insert_into_db/format_data.py
@@ -1,11 +1,12 @@
 class FormatData:
-    def __init__(self, data, attributes_to_remove):
-        self.data = data
+    def __init__(self, attributes_to_remove):
         self.attributes = attributes_to_remove
-    
-    def format(self):
-        for contributor in self.data:
-            for attribute in self.data.keys():
-                if attribute in self.attributes:
-                    del contributor[attribute]
-        return self.data
+        self.formatted_data = {}
+
+    def search_dict(self, dict_to_search):
+        for attribute in dict_to_search:
+            if type(dict_to_search[attribute]) is not type(dict()):
+                self.formatted_data[attribute] = dict_to_search[attribute]
+            else:
+                return self.search_dict(dict_to_search[attribute])
+        return self.formatted_data
--- test_scripts/test_format_data.py
+++ test_scripts/test_format_data.py
@@ -1,21 +1,33 @@
 from insert_into_db.format_data import FormatData
 
+def test_dict_one_attribute():
+    test_dict = {"test_1": "Hello"}
+    assert FormatData(["should_fail"]).search_dict(test_dict) == test_dict
+
+
+def test_dict_sub_dict():
+    test_dict = {"test_1": "Hello", "test_2":{"anAttribute": "Hello"}}
+    assert FormatData(["should_fail"]).search_dict(test_dict) == test_dict
+
 def test_format_data():
     contributor = {'form_id': '001',
-    'period': '202012',
-    'reference': '1234',
-    'responses': [{'question_code': '642', 'response': 4667},
-    {'question_code': '124', 'response': 8966},
-    {'question_code': '432', 'response': 385}],
-    'should_fail': "Overridden",
+    'period': 202012,
+    'reference': 1235,
+    'responses': {'Q146': {'response': 442, 'should_fail': True},
+    'Q201': {'response': 1663, "should_fail": False},
+    'Q202': {'response': 9387},
+    'Q203': {'response': 2775},
+    'Q204': {'response': 2477, 'should_fail': False}},
     'survey': '001'}
 
     contributor_output = {'form_id': '001',
-    'period': '202012',
-    'reference': '1234',
-    'responses': [{'question_code': '642', 'response': 4667},
-    {'question_code': '124', 'response': 8966},
-    {'question_code': '432', 'response': 385}],
+    'period': 202012,
+    'reference': 1235,
+    'responses': {'Q146': {'response': 442},
+    'Q201': {'response': 1663},
+    'Q202': {'response': 9387},
+    'Q203': {'response': 2775},
+    'Q204': {'response': 2477}},
     'survey': '001'}
 
 
